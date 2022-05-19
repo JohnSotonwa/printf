caf98c2a41334e01955770c5aa04791be0a0e33e
Sotonwa John <johnsotonwa@gmail.com>
2022-05-19 14:39:46 +0100

    Add code for string format specifier

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+* @_printf: print according to format specifiers %c & %s
+* @format: input string
+*
+* Return: print characters
+*/
+
+int _putchar(char c)
+{
+    return(write(1, &c, 1));
+}
+int _printf (const char *format, ...)
+{
+    va_list arg;
+    int i, j;
+    char *str;
+
+    va_start (arg, format);
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+        }
+        else
+        {
+            if (format[i+1] == 's')
+            {
+                i++;
+                str = va_arg(arg, char *);
+
+                while(str[j] != '\0')
+                {   _putchar(str[j]);
+                    j++;
+                }
+            }
+        }
+        i++;
+    }
+    va_end (arg);
+}
+

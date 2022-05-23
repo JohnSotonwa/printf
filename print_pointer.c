3f314ecdd199fe2fce5957bd21cb67cfbdba18eb
Sotonwa John <johnsotonwa@gmail.com>
2022-05-23 03:26:53 +0100

    Correction

--- print_pointer.c
+++ print_pointer.c
@@ -28,3 +28,22 @@ int print_pointer(va_list list)
 
 	return (size);
 }
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: String 1
+ * @s2: String 2
+ * Return: Integer
+ **/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
+}

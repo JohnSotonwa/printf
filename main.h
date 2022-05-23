8c2c20126a8653f69e5f124d6fd4da9590ec4072
Sotonwa John <johnsotonwa@gmail.com>
2022-05-23 03:37:10 +0100

    Corrections

--- main.h
+++ main.h
@@ -11,47 +11,4 @@ int _output_char(va_list arg);
 int _output_str(va_list arg);
 int (*_select_func(char c))(va_list);
 
-
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
-
-/* printf.c */
-int _printf(const char *, ...);
-
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-
-/* printers */
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
-
-/* _putchar.c */
-int _putchar(char);
-int buffer(char);
-
-/**
- * struct _format - Typedef struct
- *
- * @type: Format
- * @f: The function associated
- **/
-typedef struct _format
-{
-	char type;
-	int (*f)(va_list);
-} format;
-
-
 #endif

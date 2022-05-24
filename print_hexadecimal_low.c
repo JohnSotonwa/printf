b3aa4d931da200fb945b887e484307f8cfa3c496
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-05-24 16:52:08 +0100

    edited itoa naming

--- print_hexadecimal_low.c
+++ print_hexadecimal_low.c
@@ -11,7 +11,7 @@ int print_hexadecimal_low(va_list list)
 	char *p_buff;
 	int size;
 
-	p_buff = itoa(va_arg(list, unsigned int), 16);
+	p_buff = _itoa(va_arg(list, unsigned int), 16);
 
 	size = print((p_buff != '\0') ? p_buff : "NULL");
 

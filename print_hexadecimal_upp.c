53f9ea89157851c1cfca514535ac20004ca32c37
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-05-24 17:23:24 +0100

    itoa changed to int_to_string

--- print_hexadecimal_upp.c
+++ print_hexadecimal_upp.c
@@ -14,7 +14,7 @@ int print_hexadecimal_upp(va_list list)
 	char *p_buff;
 	int size;
 
-	p_buff = _itoa(va_arg(list, unsigned int), 16);
+	p_buff = int_to_string(va_arg(list, unsigned int), 16);
 	p_buff = string_to_upper(p_buff);
 
 	size = print((p_buff != '\0') ? p_buff : "NULL");

0e5ebb9153c0dd97b7d98eeac553e56f41006b12
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-05-24 17:22:20 +0100

    itoa changed to int_to_string

--- print_hexadecimal_low.c
+++ print_hexadecimal_low.c
@@ -11,7 +11,7 @@ int print_hexadecimal_low(va_list list)
 	char *p_buff;
 	int size;
 
-	p_buff = _itoa(va_arg(list, unsigned int), 16);
+	p_buff = int_to_string(va_arg(list, unsigned int), 16);
 
 	size = print((p_buff != '\0') ? p_buff : "NULL");
 

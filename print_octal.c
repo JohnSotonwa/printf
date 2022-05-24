92b9919dc98e45f3e07d2a4cc625fe1aa0a8f3aa
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-05-24 17:24:37 +0100

    itoa changed to int_to_string

--- print_octal.c
+++ print_octal.c
@@ -11,7 +11,7 @@ int print_octal(va_list list)
 	char *p_buff;
 	int size;
 
-	p_buff = _itoa(va_arg(list, unsigned int), 8);
+	p_buff = int_to_string(va_arg(list, unsigned int), 8);
 
 	size = print((p_buff != '\0') ? p_buff : "NULL");
 

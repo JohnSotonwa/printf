61513cc7cf63dc65b9623acd992a45972e6e747a
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-05-24 16:52:36 +0100

    edited itoa naming

--- print_hexadecimal_upp.c
+++ print_hexadecimal_upp.c
@@ -14,7 +14,7 @@ int print_hexadecimal_upp(va_list list)
 	char *p_buff;
 	int size;
 
-	p_buff = itoa(va_arg(list, unsigned int), 16);
+	p_buff = _itoa(va_arg(list, unsigned int), 16);
 	p_buff = string_to_upper(p_buff);
 
 	size = print((p_buff != '\0') ? p_buff : "NULL");

2b4e24b4c0c88613cde3b07c8a486e48f7ec3373
JohnSotonwa <102174942+JohnSotonwa@users.noreply.github.com>
2022-05-24 16:55:05 +0100

    edited itoa naming

--- print_unsigned.c
+++ print_unsigned.c
@@ -11,7 +11,7 @@ int print_unsigned(va_list list)
 	char *p_buff;
 	int size;
 
-	p_buff = itoa(va_arg(list, unsigned int), 10);
+	p_buff = _itoa(va_arg(list, unsigned int), 10);
 
 	size = print((p_buff != '\0') ? p_buff : "NULL");
 

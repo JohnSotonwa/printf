666833623240d2c082962405e85b259242f0620d
Sotonwa John <johnsotonwa@gmail.com>
2022-05-25 13:23:10 +0100

    Decimal and Integer

--- _printf.c
+++ _printf.c
@@ -13,17 +13,16 @@ int (*find_function(const char *format))(va_list)
 	code_f find_f[] = {
 		{"c", print_char},
 		{"s", print_string}, 
-		{NULL, NULL},/*
 		{"i", print_int},
 		{"d", print_dec},
-		{"r", print_rev},
+		/*{"r", print_rev},
 		{"b", print_bin},
 		{"u", print_unsig},
 		{"o", print_octal},
 		{"x", print_x},
 		{"X", print_X},
-		{"R", print_rot13},
-		{NULL, NULL}*/
+		{"R", print_rot13},*/
+		{NULL, NULL}
 	};
 
 	while (find_f[i].sc)
